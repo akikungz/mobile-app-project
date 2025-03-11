0f8b9b6a03a2cb184cea9beda63ceb39bf517787
Thitipong Tapianthong <thitipong.t@email.kmutnb.ac.th>
2025-03-11 21:12:53 +0700

    feat: add CORS support and update dependencies in server configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
+import { cors } from "@elysiajs/cors";
 
 import { users } from "@/routes/users";
 import { todos } from "@/routes/todos";
@@ -8,6 +9,10 @@ import { env } from "@/libs/env";
 
 export const app = new Elysia()
   .use(swagger())
+  .use(cors({
+    origin: "*"
+  }))
+  .onAfterResponse((ctx) => console.log(`[${ctx.request.method}] ${ctx.path} ${ctx.set.status} ${ctx.request.body?.toString()}`))
   .use(users)
   .use(todos)
   // .listen(env.PORT, ({ port }) => console.log(`Server is running on ${port}`));
